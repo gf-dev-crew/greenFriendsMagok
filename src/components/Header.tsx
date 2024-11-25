8dc9940a0662e9721dcc527afa7e53447cb19e42
kimzeze <fridaynight@kakao.com>
2024-11-25 21:50:03 +0900

    [💄style] Header 로고 및 스타일 수정

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,15 +5,11 @@ import MeatBallsMenu from './MeatBallsMenu';
 
 const Header = () => {
   return (
-    <header className="w-full border-b border-primary bg-white px-2.5 text-primary">
-      <div className="mx-auto flex h-[50px] max-w-screen-xl items-center justify-between">
-        <div className="flex items-center">
-          <Link href="/" aria-label="홈으로 이동">
-            <div className="pt-[4px]">
-              <Image src="/assets/Logo.svg" alt="녹색친구들 마곡 로고" width={244} height={25} priority />
-            </div>
-          </Link>
-        </div>
+    <header className="fixed top-0 z-50 w-full bg-white/80 px-[24px]">
+      <div className="flex h-[50px] items-center justify-between">
+        <Link href="/" aria-label="홈으로 이동" className="flex items-center">
+          <Image src="/assets/Logo.svg" alt="Nook 로고" width={100} height={25} priority />
+        </Link>
         <MeatBallsMenu />
       </div>
     </header>

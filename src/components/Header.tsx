9a79336d591de2005d46ff449fc2e6b752bd663d
kimzeze <fridaynight@kakao.com>
2024-11-25 22:03:34 +0900

    [✨feat] Header 상단 스크롤에 고정

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 
 const Header = () => {
   return (
-    <header className="fixed top-0 z-50 w-full bg-white/80 px-[24px]">
+    <header className="fixed left-1/2 top-0 z-50 w-full max-w-[480px] -translate-x-1/2 bg-white/80 px-[24px]">
       <div className="flex h-[50px] items-center justify-between">
         <Link href="/" aria-label="홈으로 이동" className="flex items-center">
           <Image src="/assets/Logo.svg" alt="Nook 로고" width={100} height={25} priority />

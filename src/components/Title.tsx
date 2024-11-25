73b808d858d1a5bd5b43c89917bfb4ad0775d3d4
kimzeze <fridaynight@kakao.com>
2024-11-25 22:01:49 +0900

    [💄style] 제목 컴포넌트 스타일 수정

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -5,20 +5,14 @@ interface TitleProps {
   title: string;
   href: string;
   isMainPage: boolean;
-  className?: string; // 선택적 props로 추가 클래스네임을 받습니다.
+  className?: string;
 }
 
 export default function Title({ title, href, isMainPage, className }: TitleProps) {
   return (
     <Link href={href} className={cn('block', className)}>
-      <div className="flex cursor-pointer flex-row items-center justify-between py-2">
-        <h3
-          className={cn(
-            'font-semibold',
-            isMainPage ? 'text-2xl' : 'mb-3 text-3xl',
-            className, // 추가 클래스네임을 h3 태그에도 적용합니다.
-          )}
-        >
+      <div className="flex flex-row items-center justify-between py-2">
+        <h3 className={cn('font-semibold', isMainPage ? 'cursor-pointer text-2xl' : 'mb-3 text-3xl', className)}>
           {title}
         </h3>
         {isMainPage && (

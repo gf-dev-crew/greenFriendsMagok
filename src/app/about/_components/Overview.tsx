71c2cd05e30eb560ef829b9f163b0c6399c45a6c
kimzeze <fridaynight@kakao.com>
2024-11-25 22:02:49 +0900

    [💄style] Overview 컴포넌트 스타일 수정

--- src/app/about/_components/Overview.tsx
+++ src/app/about/_components/Overview.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image';
 
 export default function Overview() {
   return (
-    <div className="mb-4 flex cursor-pointer flex-col items-center justify-between py-2">
+    <div className="mb-4 flex flex-col items-center justify-between py-2">
       <h3 className="mb-3 w-full text-left text-2xl font-semibold">Overview</h3>
       <Image src={'/assets/overviewSVG.svg'} alt="건물 건체 안내 이미지" width={600} height={961} />
     </div>

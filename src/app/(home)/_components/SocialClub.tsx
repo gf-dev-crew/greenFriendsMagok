6502ef6f8e07eea2ca82ed8f259c43d0945ef197
kimzeze <fridaynight@kakao.com>
2024-11-25 22:07:03 +0900

    [🗼markup] 소셜 클럽 컴포넌트 임시 제작

---
+++ src/app/(home)/_components/SocialClub.tsx
@@ -0,0 +1,9 @@
+export default function SocialClub() {
+  return (
+    <div className="mb-5 flex h-[300px] w-full flex-col py-2 text-2xl font-semibold">
+      <div className="h-[300px] w-full border border-primary bg-gray-100">
+        {/* <Image src={'/assets/heroImage.png'} layout="full" width={600} height={900} alt="녹색친구들 마곡 메인 이미지" /> */}
+      </div>
+    </div>
+  );
+}

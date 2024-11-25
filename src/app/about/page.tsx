1f35d07bd7fd2613a645d9dde9c0e9a94c18fec4
kimzeze <fridaynight@kakao.com>
2024-11-25 22:05:37 +0900

    [🗼markup] About 페이지 수정

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import Title from '@/components/Title';
 import Overview from './_components/Overview';
 import SpaceDesign from './_components/SpaceDesign';
+import Introduction from './_components/Introduction';
 
 export default function AboutPage() {
   return (
     <main className="">
       <Title title="About Us" href="/about" isMainPage={false} />
+      <Introduction />
       <Overview />
       <SpaceDesign />
     </main>
